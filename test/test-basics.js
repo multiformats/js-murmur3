dbbdf550c67300535073bda8b90759d385d609f4
Rod Vagg <rod@vagg.org>
2021-07-16 10:42:54 +1000

    feat!: add types, update deps, semantic-release, dependabot

--- test/test-basics.js
+++ test/test-basics.js
@@ -1,23 +1,40 @@
-/* globals describe, it */
-import main from '@multiformats/murmur3'
-import assert from 'assert'
-import multiformats from 'multiformats/basics'
-const test = it
-const same = assert.deepStrictEqual
+/* eslint-env mocha */
+import * as murmur3 from '@multiformats/murmur3'
+import chai from 'chai'
+import { bytes } from 'multiformats'
 
-const { bytes } = multiformats
-const { multihash } = multiformats
-multihash.add(main)
+const { assert } = chai
 
-const fixture = bytes.fromString('testFixture')
+const fixtures = [
+  ['murmur3-32', 'beep boop', '2304243ddb9e'],
+  ['murmur3-128', 'beep boop', '2210acfe9c5bbf88f075c0c4df0464430ead']
+]
 
-for (const { name, code } of main(multiformats)) {
-  describe(name, () => {
-    test('encode', async () => {
-      const hashed = await multihash.hash(fixture, name)
-      await multihash.validate(hashed, fixture)
-      const duplicate = await multihash.hash(fixture, code)
-      same(hashed, duplicate)
+describe('Digests', () => {
+  for (const [name, input, expectedDigest] of fixtures) {
+    it(name, async () => {
+      const hasher = murmur3[name.replace('-', '')]
+      const hash = await hasher.digest(bytes.fromString(input))
+      assert.strictEqual(hash.code, hasher.code)
+      assert.strictEqual(bytes.toHex(hash.bytes), expectedDigest)
     })
+  }
+})
+
+describe('Hashers', () => {
+  const fixture = bytes.fromString('testFixture')
+
+  it('murmur3-32', async () => {
+    const hash = await murmur3.murmur332.digest(fixture)
+    assert.strictEqual(murmur3.murmur332.code, 0x23)
+    assert.strictEqual(hash.code, murmur3.murmur332.code)
+    assert.strictEqual(bytes.toHex(hash.bytes), '2304e7285ca1')
   })
-}
+
+  it('murmur3-128', async () => {
+    const hash = await murmur3.murmur3128.digest(fixture)
+    assert.strictEqual(murmur3.murmur3128.code, 0x22)
+    assert.strictEqual(hash.code, murmur3.murmur3128.code)
+    assert.strictEqual(bytes.toHex(hash.bytes), '2210df48782b0b497325f116d6589ef4c112')
+  })
+})
