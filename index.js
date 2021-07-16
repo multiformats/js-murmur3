a2ecaddbdba4f611af8c3dfdc85e3025af81a4e5
Rod Vagg <rod@vagg.org>
2021-07-16 10:42:54 +1000

    feat!: add types, update deps, semantic-release, dependabot

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import { from } from 'multiformats/hashes/hasher'
+import { bytes } from 'multiformats'
+// @ts-ignore
 import mur from 'murmurhash3js-revisited'
 
-const fromNumberTo32BitBuf = number => {
+/**
+ * @param {number} number
+ * @returns {Uint8Array}
+ */
+function fromNumberTo32BitBuf (number) {
   const bytes = new Array(4)
   for (let i = 0; i < 4; i++) {
     bytes[i] = number & 0xff
@@ -9,14 +16,14 @@ const fromNumberTo32BitBuf = number => {
   return new Uint8Array(bytes)
 }
 
-const create = multiformats => {
-  const { bytes } = multiformats
-  const { fromHex } = bytes
-  const hashes = [
-    { name: 'murmur3-128', code: 0x22, encode: data => fromHex(mur.x64.hash128(data)) },
-    { name: 'murmur3-32', code: 0x23, encode: data => fromNumberTo32BitBuf(mur.x86.hash32(data)) }
-  ]
-  return hashes
-}
+export const murmur332 = from({
+  name: 'murmur3-32',
+  code: 0x23,
+  encode: (input) => fromNumberTo32BitBuf(mur.x86.hash32(input))
+})
 
-export default create
+export const murmur3128 = from({
+  name: 'murmur3-128',
+  code: 0x22,
+  encode: (input) => bytes.fromHex(mur.x64.hash128(input))
+})
