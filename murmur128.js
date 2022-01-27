18ef28df393a2b1b085608d1ad5f3869fef254c6
Irakli Gozalishvili <contact@gozala.io>
2022-01-26 17:34:01 -0800

    feat: implement sync multihash hasher and export encode methods (#17)

---
+++ murmur128.js
@@ -0,0 +1,21 @@
+import { mur } from './deps.js'
+import { bytes } from 'multiformats'
+import * as Digest from 'multiformats/hashes/digest'
+
+export const name = 'murmur3-128'
+export const code = 0x22
+
+/**
+ * @param {Uint8Array} input
+ * @returns {import('multiformats/hashes/interface').MultihashDigest<typeof code>}
+ */
+export const digest = (input) => {
+  return Digest.create(code, encode(input))
+}
+
+/**
+ * @param {Uint8Array} input
+ */
+export const encode = (input) => {
+  return bytes.fromHex(mur.x64.hash128(input))
+}
