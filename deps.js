d81d7ff3eb32e1a0aabc92982f9d9a0ee3d76222
Irakli Gozalishvili <contact@gozala.io>
2022-02-03 15:17:25 -0800

    chore: elaborate on ts-nocheck

--- deps.js
+++ deps.js
@@ -1,3 +1,6 @@
-// @ts-nocheck - this will be overlayed
+// @ts-nocheck
+// Exported library has no type defs which makes TS unhappy. To workaround this
+// we define `deps.ts` and ignore this file. That way TS picks types and provides
+// inference.
 import mur from 'murmurhash3js-revisited'
 export { mur }
